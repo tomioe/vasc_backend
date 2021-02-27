69bd09852988caa64599bdfd4c91d305231569ff
Tobias Østergren <tobias.oestergren@ieee.org>
2021-02-27 15:42:10 +0100

    added initial non-persistant tracker

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express =  require("express");
 
+
 const bodyParser =  require("body-parser");
 const serverCrypto =  require("crypto");
+const Buffer = require('safe-buffer').Buffer
 const sharp = require('sharp')
 const cors =  require("cors");
 const path =  require("path");
+
+
 const fs = require("fs");
+
 
 const util = require("util");
 
@@ -85,6 +90,46 @@ app.get("/image/:imageName", (req, res) => {
     }
 });
 
+// we use this url when we want to track the user going to a vendor's page
+// initially we want to log this:
+//      /click?url=[base64(url)]
+// later, we should include things like client-browser, width/height, etc. from client page.
+// we can encode this JSON object in base64 as well
+app.get("/click", (req, res) => {
+    //console.log("[API] Tracking started.")
+
+    // step 0: decode the URL
+    //console.log("[API] Decoding URL...")
+    const urlBase64 = req.query["url"];
+    if(urlBase64 === "") {
+        //console.log("[API] Passed URL was empty, returning!")
+        return;
+    }
+    const urlBuffer = new Buffer(urlBase64, 'base64');
+    const urlDecoded = urlBuffer.toString();
+
+    //console.log("[API] Extracting IP...")
+    // step 1: extract information about the request [ip]
+    const ip_addr = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress; // https://stackoverflow.com/posts/23835670/revisions
+    const requestUserInformation = {
+        "ip_addr": ip_addr,
+        "time": new Date()
+    }
+    //console.log("[API] Done:")
+    console.log(requestUserInformation)
+
+    // step 2: async entry into DB, with additional info (datetime + geoloc[?]) [https://rapidapi.com/blog/geolocation-backend-node-express/]
+    console.log("[API] URL Decoded, IP address extracted, Entering information into database...")
+    const enterTrackData = async (addressToTrack,userInformation) => {
+        await console.log(requestUserInformation)
+        await console.log("[API] Information entered successfully!")
+    };
+    enterTrackData();
+
+    // step 3: redirect to the url
+    console.log("[API] Redirecting to requested page.")
+    res.redirect(urlDecoded);
+})
 
 //Catches requests made to / [root]
 app.get("/", (req, res) => {
